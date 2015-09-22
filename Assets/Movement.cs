67126d706a1d9f01f91f570e15003b0d6071773b
JiaYan Hu <jiayan.a.hu@gmail.com>
2015-09-22 01:00:43 -0400

    player now moves by point click, and added skeleton pack assets

---
+++ Assets/Movement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	#region constants
+	const int _MAX_RAYCAST_DISTANCE = 1000; 
+	const int _PLAYER_TURN_SPEED = 12;
+	#endregion 
+
+	#region vars
+	public float speed;
+	public CharacterController characterController;
+
+	Vector3 _mousePosition;
+	#endregion
+
+	#region methods
+	// Use this for initialization
+	void Start () {
+		_mousePosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			//lcoate position of the mouse
+			locatePosition();
+		}
+		moveToMousePosition();
+	}
+
+	/// <summary>
+	/// Locates the position of the mouse click and returns it. 
+	/// If no raycast detection, return the player's position
+	/// </summary>
+	/// <returns>The position.</returns>
+	void locatePosition() {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		//structure used to get information back from a raycast
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, _MAX_RAYCAST_DISTANCE)) {
+			//retrieve coordinates from raycast and store it as position
+			_mousePosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+		}
+	}
+
+	/// <summary>
+	/// Moves player to mouse position
+	/// </summary>
+	void moveToMousePosition() {
+		if (Vector3.Distance (_mousePosition, transform.position) > 1) {
+			//get the direction player should be facing
+			Quaternion faceDirection = Quaternion.LookRotation (_mousePosition - transform.position);
+			
+			//only rotate y 
+			faceDirection.x = 0f;
+			faceDirection.z = 0f;
+			//face the direction to move to
+			transform.rotation = Quaternion.Slerp (transform.rotation, faceDirection, Time.deltaTime * _PLAYER_TURN_SPEED);
+			
+			//move player
+			characterController.SimpleMove (transform.forward * speed); 
+		}
+	}
+	#endregion
+}
