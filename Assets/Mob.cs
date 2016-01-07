cc68650708a272505afeee9d60d46ef4fc16fbaa
JiaYan Hu <jiayan.a.hu@gmail.com>
2016-01-07 12:32:03 -0500

    script for mob movement/chase

---
+++ Assets/Mob.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob: MonoBehaviour {
+	#region vars
+	public float speed; 
+	public float range;
+	public CharacterController controller; 
+
+	public AnimationClip run;
+	public AnimationClip idle;
+
+	public Transform player; 
+	#endregion
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log (inRange ()); 
+		if (!inRange ()) {
+			chase ();
+		} else {
+			animation.CrossFade(idle.name);
+		}
+	}
+
+	bool inRange() {
+		if (Vector3.Distance(player.transform.position, this.transform.position) > range) {
+			return false; 
+		}
+
+		return true; 
+	}
+
+	void chase() {
+		//turn 
+		transform.LookAt (player.position);
+		controller.SimpleMove (transform.forward * speed); 
+		//animation
+		animation.CrossFade (run.name);
+	}
+}
